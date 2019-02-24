3955ac0a22f2d5c29e1adcccc9677014018fb491
amliulibo <20224151@qq.com>
2019-02-24 17:51:58 +0800

    add

---
+++ ActiveMQDemo/test/demo/demo1/testAMQQueue.java
@@ -0,0 +1,82 @@
+package demo.demo1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class testAMQQueue {
+	Map<String, String> bankMap=new HashMap<>();
+	@Before
+	public void Init()
+	{
+		bankMap.put("bank01", "tcp://localhost:61616");
+		bankMap.put("bank02", "tcp://localhost:61616");
+		bankMap.put("bank03", "tcp://localhost:61616");
+	}
+	//@Test
+	public void test01Consumer() throws JMSException
+	{
+		for (String bank : bankMap.keySet()) {
+			new AMQQueueConsumer(bank, bankMap.get(bank));
+		}
+		
+	}
+	@Test
+	public void test01Consumer1() throws JMSException
+	{
+		for (String bank : bankMap.keySet()) {
+			new AMQQueueConsumer(bank, bankMap.get(bank),new MessageListener() {
+				
+				@Override
+				public void onMessage(Message message) {
+					 if(message instanceof TextMessage){
+		                 TextMessage textMessage = (TextMessage)message;
+		               
+		                 
+		                 try {
+		                	//确认接收，并成功处理了消息
+		                	 message.acknowledge();
+		                     System.out.println("<<<<<<<<接收到消息:msg="+ textMessage.getText());
+		                 } catch (JMSException e) {
+		                     // TODO Auto-generated catch block
+		                     e.printStackTrace();
+		                 }
+		             }
+					
+				}
+			});
+		}
+		
+	}
+	@Test
+	public void test02Producer()
+	{
+		for (String bank : bankMap.keySet()) {
+			for (int i = 0; i < 3; i++) {
+				try {
+					new AMQQueueProducer(bank, bankMap.get(bank))
+					.sendQueueMsg(bank+String.valueOf(i));
+					Thread.sleep(1000);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+
+}
---
+++ ActiveMQDemo/test/demo/demo2/testMultiBank.java
@@ -0,0 +1,84 @@
+package demo.demo2;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.springframework.jms.connection.CachingConnectionFactory;
+
+
+
+
+public class testMultiBank {
+
+	Map<String, String> bankMap=new HashMap<>();
+	
+	@Before
+	
+	public void InitTest()
+	{
+		bankMap.put("bank01", "192.168.10.10");
+		bankMap.put("bank02", "192.168.10.11");
+		bankMap.put("bank03", "192.168.10.12");
+	}
+	
+	@Test
+    public void testSendMessage() throws InterruptedException {
+		
+		for (String bank : bankMap.keySet()) {
+			 
+	        for (int idx = 1; idx <= 3; idx++) {
+	            
+	            /*
+	             * 生产者 发布 消息到 queue/queue_b 的队列中
+	             */
+	        	String mString=String.valueOf(idx * 1111);
+	        	System.out.println("队列"+bank+ "发送消息:"+mString);
+	            JMSPublisher.sendQueueMessage(bank,mString);
+	            Thread.sleep(1000);
+	            
+	            
+	        }
+		}
+        assertEquals("1", "1");
+        
+        
+    }
+
+	@Test
+    public void receiver() {
+		for (String bank : bankMap.keySet()) {
+			/*
+	         * 消费者 订阅队列  queue/queue_b 是否有消息发布，有侧打印出来  （通过 onMessage 监听）
+	         */
+	       JMSListener.startJmsQueueListener(bank ,new MessageListener() {
+	            @Override
+	             public void onMessage(Message message) {
+	                try {
+	                    if (message instanceof TextMessage) {
+	                        TextMessage msg = (TextMessage) message;
+	                        System.out.println("== 收到一个JMS消息..." + msg.getText());
+	                    } 
+	                } catch (JMSException e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	        });
+		}
+        
+        
+        assertEquals("1", "1");
+		//System.in.read();
+    }
+
+}
