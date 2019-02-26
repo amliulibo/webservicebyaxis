5516b44646f73eecbf8e76e92502ea7dd70f68e5
amliulibo <20224151@qq.com>
2019-02-26 11:22:33 +0800

    ok1

--- webdemo/src/main/java/demo2/Worker.java
+++ webdemo/src/main/java/demo2/Worker.java
@@ -1,11 +1,53 @@
 package demo2;
 
- 
- 
- 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
+
 public class Worker
 {
 
-	  
+	private final static String QUEUE_NAME="QUEUE2";
+	public static void main(String[] args) throws IOException, TimeoutException {
+		// TODO Auto-generated method stub
+		ConnectionFactory factory=new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection=factory.newConnection();
+		Channel channel=connection.createChannel();
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		
+		System.out.println("[*] waiting for message,To exit press CTRL+C");
+		
+		DeliverCallback deliverCallback=(consumerTag,delivery)->{
+			String message=new String(delivery.getBody(),"UTF-8");
+			
+			try {
+				doWork(message);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			
+			System.out.println("[X] received"+message);
+		};
+		
+		
+		
+		channel.basicConsume(QUEUE_NAME, true,deliverCallback,consumerTag->{});
+		
+	}
+	private static void doWork(String task) throws InterruptedException
+	{
+		for (char ch : task.toCharArray()) {
+			if (ch=='.') {
+				Thread.sleep(1000);
+			}
+		}
+	}
+
 
 }
